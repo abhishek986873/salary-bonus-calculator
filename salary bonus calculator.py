9a23931d8fc0a1cc458b8503d2a995a85b8e8abf
abhishek986873 <110660543+abhishek986873@users.noreply.github.com>
2022-08-21 12:14:50 +0530

    Add files via upload

---
+++ salary bonus calculator.py
@@ -0,0 +1,13 @@
+print('-----------------------------------------------------------------')
+print('* INSTRUCTIONS')
+print('----------------------------------------------------------------')
+print("* TO GET BONUS YOUR SERVICE YEAR MUST GREATER THAN 5 YEAR")
+user_salary=int(input('* ENTER YOUR SLARY: '))
+user_service_year=int(input('* ENTER YOUR SERVICE YEAR: '))
+bonus_amount=(5/100)*user_salary
+final_amount=bonus_amount+user_salary
+if (user_service_year>5):
+    print(final_amount)
+else:
+    print('• Sorry Your Service Year Is Less Than 5 Year')
+    print('• Your Do Not Get Any Bonus')
